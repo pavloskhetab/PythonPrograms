8f0a8413c820e210fc4944ab54de4215f88f1c51
Pavlos Khetab <pavlos.khetab@gmail.com>
2022-10-20 13:15:13 +0100

    Exercise 11 Asking Questions

---
+++ ex11.py
@@ -0,0 +1,16 @@
+# Asking Questions
+print("How old are you?", end = ' ')
+age = input()
+print("How tall are you? (in cm)", end = ' ')
+height = input()
+print("How much do you weigh?(in kg)", end = ' ')
+weight = input()
+
+print(f"So you're {age} years old, {height}cm tall and {weight}kg heavy.")
+
+"""
+To get a number as an input
+for arithmetic calculations
+we use
+x = int(input())
+"""
