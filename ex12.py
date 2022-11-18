3547fb738b58ea626032564ead1df17a1db69be9
Pavlos Khetab <pavlos.khetab@gmail.com>
2022-11-18 17:05:59 +0000

    Exercise 12 Prompting People

---
+++ ex12.py
@@ -0,0 +1,6 @@
+# Prompting People
+age = input("How old are you? ")
+height = input("How tall are you? (in cm) ")
+weight = input("How much do you weigh?(in kg) ")
+
+print(f"So you're {age} years old, {height}cm tall and {weight}kg heavy.")
